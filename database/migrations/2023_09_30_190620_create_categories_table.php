feb1cfc7df3a93254ca3ebc34d85a3e483d6ecac
ahmedtamee <123337830+tameesoul@users.noreply.github.com>
2023-09-30 22:31:02 +0300

    setting up database

--- database/migrations/2023_09_30_190620_create_categories_table.php
+++ database/migrations/2023_09_30_190620_create_categories_table.php
@@ -11,14 +11,17 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('parent_id')
+            ->nullable()
+            ->constrained('categories','id')
+            ->nullOnDelete();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('phone_number')->nullable();
-            $table->rememberToken();
+            $table->string('slug')->unique();
+            $table->text('description')->nullable();
+            $table->string('image')->nullable();
+            $table->enum('status',['active,archived']);
             $table->timestamps();
         });
     }
@@ -28,6 +31,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('categories');
     }
 };
